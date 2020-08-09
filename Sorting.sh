1d2c9e93b0108701b696f38982b35570807a355f
Punam <punamumare25@gmail.com>
2020-08-09 13:55:27 +0530

    sorting Arithmetic Computation Problem

---
+++ Sorting.sh
@@ -0,0 +1,53 @@
+#!/bin/bash -x
+
+echo "Welcome to Arithmetic Computation and Sorting."
+
+read -p "Enter three Values: " a b c
+echo "$a $b $c"
+
+declare -A DictResult
+DictResult[Exp1]=$((a+b*c))
+DictResult[Exp2]=$((a*b+c))
+DictResult[Exp3]=$((c+a/b))
+DictResult[Exp4]=$((a%b+c))
+echo "${DictResult[@]}"
+
+#Read Values from Dictionary to array
+for (( counter=1; counter<=4; counter++ ))
+do
+	arrayResult[$counter]=${DictResult[Exp$counter]}
+done
+
+echo "${arrayResult[@]}"
+
+#Descending Order
+for (( counter1=1; counter1<=4; counter1++ )) 
+do
+	for (( counter2=1; counter2<4; counter2++ ))
+	do
+		if [[ ${arrayResult[$counter2]} -lt ${arrayResult[$(($counter2+1))]} ]]
+		then
+			temp=${arrayResult[$counter2]}
+    		arrayResult[$counter2]=${arrayResult[$((counter2+1))]}
+			arrayResult[$((counter2+1))]=$temp
+		fi
+	done
+done
+
+echo "${!arrayResult[@]} : ${arrayResult[@]} "
+
+#Ascending Order
+for (( counter1=1; counter1<=4; counter1++ ))
+do
+   for (( counter2=1; counter2<4; counter2++ ))
+   do
+      if [[ ${arrayResult[$counter2]} -gt ${arrayResult[$(($counter2+1))]} ]]
+      then
+         temp=${arrayResult[$counter2]}
+         arrayResult[$counter2]=${arrayResult[$((counter2+1))]}
+         arrayResult[$((counter2+1))]=$temp
+      fi
+   done
+done
+
+echo "${!arrayResult[@]} : ${arrayResult[@]} "
